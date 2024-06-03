c213d5a8b0395d5f77b6e14f16eac675a63e961c
xenongee <rublgleb@gmail.com>
2024-06-03 11:56:34 +0300

    Variables and values

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hello World, i love you");
+const userName = 'Gleb';
+let userAge = 23;
+let something = 0;
+console.log(userName, userAge, something);
+// userName = 'Alexey'; - cannot assign because it is const
+userAge = 24;
+something = 'hi';
+console.log(userName, userAge, something);
