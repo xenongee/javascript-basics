c9ceae2c40623b8e057b64bb5922a243808f8995
xenongee <rublgleb@gmail.com>
2024-06-03 12:15:20 +0300

    Assignment and comparison operators

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("Hello World, i love you");
+// console.log("Hello World, i love you");
 
 /* 
     Variables and values
-*/
 
 const userName = 'Gleb';
 let userAge = 23;
@@ -14,10 +13,10 @@ something = 'hi';
 // console.log(userName, userAge, something);\
 
 console.log(userName + ', age: ' + userAge + ', something: ' + something);
+*/
 
 /* 
     Arithmetic operators
-*/
 
 const width = 5;
 const height = 10;
@@ -31,4 +30,33 @@ console.log('width2:', width2, 'width3:',width3);
 const division = width2 / width3;
 const volume = 2**3 // 2*2*2
 console.log('division:', division, 'volume:', volume);
+*/
 
+/*
+    Assignment and comparison operators
+*/
+
+// Assignment operators
+let num = 10+5;
+num += 3; // num = num + 3
+num -= 2; // num = num - 2
+num *= 5; // num = num * 5
+num /= 10; // num = num / 10
+num %= 10; // num = num % 10
+num++; // num = num + 1
+num--; // num = num - 1
+
+console.log(num);
+
+// Comparison operators
+console.log(10 == 10); // true
+console.log(10 === 10); // true
+console.log(10 == '10'); // true
+console.log(10 == '11'); // false
+console.log(10 === '10'); // false
+console.log(10 != 10); // false
+console.log(10 !== 10); // false
+console.log(13 > 10); // true
+console.log(15 < 10); // false
+console.log(13 >= 10); // true
+console.log(15 <= 10); // false
