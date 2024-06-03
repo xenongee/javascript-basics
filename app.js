dd4ada2425079154c8d1924b856caf15609b6521
xenongee <rublgleb@gmail.com>
2024-06-03 13:51:09 +0300

    Template literals

--- app.js
+++ app.js
@@ -78,7 +78,6 @@ console.log(a, b);
 
 /*
     Data types
-*/
 
 // primitive types
 const number = 18;
@@ -97,3 +96,52 @@ const person = {
     age: 24
 };
 console.log(person, typeof person);
+*/
+
+/*
+    Exercise - Project Calculation
+
+// Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
+// К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+// Выведете в консоль:
+// Boolean переменную успеете ли вы взяться за работу
+// Сколько вы за неё попросите?
+
+// Your hourly rate is $80 and you are willing to work no more than 5 hours a day, 5 days a week (except weekends).
+// A customer comes to you and offers you an order for 40 hours of work. It's Monday. You have to leave in 11 days.
+// You'll be in the console:
+// Boolean variable whether you will have time to take the job.
+// How much will you ask for it?
+
+const workHoursPerDay = 5;
+const workDaysPerWeek = 5;
+const dayOffs = 7 - workDaysPerWeek; // 2
+const daysToLeave = 11;
+
+const hourlyRateUSD = 80;
+const projectHours = 40;
+
+const calculatedTimeForWork = workHoursPerDay * (daysToLeave - dayOffs); // 45
+
+const answer1 = projectHours <= calculatedTimeForWork;
+const answer2 = projectHours * hourlyRateUSD;
+console.log(answer1, answer2);
+
+*/
+
+/*
+    Template literals
+*/
+
+let projectName = 'HTML Company';
+let price = 1000;
+price = `${price} rub.`;
+let author = 'Vasiliy';
+
+const oldTemplate = 'Project name: ' + projectName + ', price: ' + price + ', author: ' + author;
+const template = `Project name: ${projectName}, price: ${price}, author: ${author}`;
+const template2 = 
+`Project name: ${projectName}
+Price: ${price}
+Author: ${author}`
+console.log(template2);
