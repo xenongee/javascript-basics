fbc66ff9160078043c7b1e5fa12c6c193cf9cefb
xenongee <rublgleb@gmail.com>
2024-06-04 16:46:29 +0300

    Array element management

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* 
     Arrays
-*/
 
 const roles = ['admin', 'user', 'guest'];
 
@@ -20,4 +19,42 @@ console.log(info);
 
 const someInfo = new Array('Iron', 'Wood', 2024 - 1991, 10 > 5 ? 'yes' : 'no');
 console.log(someInfo);
+*/
+
+/*
+    Array element management
+*/
+
+const users = ['John', 'Pavel', 'Mark'];
+console.log(users);
+
+users[2] = 'Katya';
+console.log(users);
+
+// добавление элементов - adding elements
+// может создать пустые элементы если индекс не следующий за последним
+// сan create empty elements if the index is not next to the last one
+users[3] = 'Alexey';
+console.log(users);
+
+// изменяет ориггинальный массив и не создает пустые элементы, возвращает новую длину
+// modifies the original array, does not create empty elements; returns new length of the array
+users.push('Olga');
+console.log(users);
+
+// добавляет элемент в начало массива, сдвигает индексы существующих элементов
+// add element to the beginning of the array, shifts the indexes of existing elements
+users.unshift('Vasiliy');
+console.log(users);
+
+// удаление элементов - deleting elements
+// удаляет последний элемент, также его и возвращает
+// deletes the last element
+users.pop();
+console.log(users);
+
+// удаляет первый элемент, возвращает удаленный элемент и сдвигает индексы
+// deletes the first element, returns the deleted element and shifts the indexes
+users.shift();
+console.log(users);
 
