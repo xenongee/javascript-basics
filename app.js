ccfe63c4b6ca08cec990c44e39beeea551cb12fc
xenongee <rublgleb@gmail.com>
2024-06-04 14:52:50 +0300

    Functions

--- app.js
+++ app.js
@@ -64,7 +64,6 @@ console.log(filename); // music.mp3
 
 /*
     Nullish coalescing operator
-*/
 
 const username = '';
 console.log(username || 'Guest'); // Guest
@@ -72,4 +71,49 @@ console.log(username ?? 'Guest'); // ''
 let age = 0;
 console.log(age || 18); // 18
 console.log(age ?? 18); // 0
+*/
 
+/*
+    Exercise - Verification of rights
+
+// Определить, может ли пользователь купить игру в интернет-магазине (например, Steam) на основании четырех условий.
+//
+//  Баланс пользователя более 1000 рублей или более 100 бонусов.
+//  Пользователь не забанен в магазине.
+//  Игра не куплена ранее пользователем.
+//  Игра доступна в продаже.
+
+// Determine whether a user can buy a game from an online store (e.g., Steam) based on four conditions.
+//
+//  User's balance is more than 1000 rubles or more than 100 bonuses.
+//  The user is not banned in the store.
+//  The game has not been previously purchased by the user.
+//  The game is available for sale.
+
+let balance = 1200;
+let bonus = 100;
+let magazineBan = false;
+let gamePurchased = false;
+let gameAvailable = true;
+
+console.log(`User can buy game: ${(balance > 1000 || bonus > 100) && !magazineBan && !gamePurchased && gameAvailable}`);
+*/
+
+/* 
+    Functions
+*/
+
+function logName(name, surname) {
+    console.log(name, surname);
+}
+
+logName('Oleg', 'Ivanov');
+
+const x = logName('Fedor', 'Petrov');
+console.log(typeof x);
+
+function countDepositSum(depositUSD, month, rate) {
+    return depositUSD * (1 + rate / 12) ** month;
+}
+
+console.log(countDepositSum(1000, 12, 0.12));
