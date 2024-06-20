8fe38c889e6e71c6e49fa8ac727482fed0066a04
xenongee <rublgleb@gmail.com>
2024-06-20 16:42:49 +0300

    Callback function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
     First class functions vs. Higher order functions
     Функции певого класса (классика программирования) vs. Функции высшего порядка (из мира функционального программирования)
 
-*/
 
 const someFunction = (x) => x*=16 ; // храниение функции (стрелочной) в переменной (store function (arrow) in variable)
 
@@ -15,3 +14,27 @@ function add(x, y) { // передача функции в качестве па
 }
 
 console.log(add(someFunction(4), -32));  // 32
+*/
+
+/*
+    Callback function
+    Функция обратного вызова
+*/
+
+function add(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function calculate(x, y, callback) {
+    console.log(callback.name);
+    
+    return callback(x, y);
+}
+
+console.log(calculate(3, 5, add));
+console.log(calculate(3, 5, sub));
+
