2ddfcc929cd60a96c019a951eff58227d3bf619b
xenongee <rublgleb@gmail.com>
2024-07-09 17:02:57 +0300

    Addressing elements

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /*
     Объекты
     Objects
-*/
 
 const userArr = ['John', 'Doe', 25, 'male'];
 const user = {
@@ -18,3 +17,23 @@ user['skills'] = ['html', 'css', 'js'];
 user[1] = 'Hello';
 
 console.log(user); // { name: 'John', surname: 'Doe', age: 25, gender: 'male', skills: [ 'html', 'css', 'js' ], 1: 'Hello' }
+*/
+
+/*
+    Обращение к элементам
+    Addressing elements
+*/
+
+const user = {
+    name: 'John',
+    surname: 'Doe',
+    age: 25,
+    gender: 'male',
+    eduBasic: 'School',
+    eduAdvanced: 'University',
+};
+
+console.log(user.name, user.surname); // John Doe
+console.log(user['name'], user['surname']); // John Doe
+const eduType = 'Advanced';
+console.log(user['edu' + eduType]); // University
