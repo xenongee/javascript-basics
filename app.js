8815e46648d55a643fefdfa5da713d4e7d13ffc6
xenongee <rublgleb@gmail.com>
2024-07-09 16:28:38 +0300

    String conversion

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
     Базовые методы
     Basic methods
-*/
 
 const user = 'John Doe';
 console.log(user[0]); // J
@@ -18,3 +17,25 @@ console.log(user.includes('Doe')); // true
 
 console.log(user.slice(0, 4)); // John
 console.log(user.slice(5, 8)); // Doe
+*/
+
+/*
+    Преобразование строк
+    String conversion
+*/
+
+const string = 'John Doe';
+console.log(string.includes('Doe')); // true
+console.log(string.startsWith('John')); // true
+console.log(string.endsWith('Doe')); // true
+console.log(new String('John Doe').includes('Doe')); // true
+
+console.log(string.toLowerCase); // john doe
+console.log(string.toUpperCase); // JOHN DOE
+console.log(string.replace('e', 'n')); // John Don
+console.log(string.replaceAll('o', 'a')); // Jahn Dan
+console.log(string.replace(/o/g, 'a')); // Jahn Dan (regex alternative)
+
+const string2 = '  Hello World       \n';
+console.log(string2); // ' Hello World       '
+console.log(string2.trim()); // 'Hello World'
