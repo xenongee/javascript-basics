00738fdfd0fc5d53dec1156f4323d9b903ae4a78
xenongee <rublgleb@gmail.com>
2024-07-09 13:23:25 +0300

    map loop

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /*
     forEach
-*/
 
 let score = [1, 3, 5, 7, 9];
 
@@ -19,3 +18,29 @@ score.forEach(el => console.log(el));
 
 console.log('forEach with index');
 score.forEach((el, index) => console.log(index, el));
+*/
+
+let score = [1, 3, 5, 7, 9];
+console.log('forEach with index and print array');
+score.forEach((el, index, score) => console.log(index, el, score));
+
+/* 
+    map
+*/
+
+let transactionUSD = [10, -5, 20, 15, -10];
+let transactionRUB = [];
+
+transactionUSD.forEach(el => {
+    transactionRUB.push(el * 90);
+});
+
+console.log(transactionRUB);
+
+console.log('map');
+
+let transactionRUB2 = transactionUSD.map((el, i) => { 
+    console.log(el, i);
+    return el * 90;
+});
+console.log(transactionRUB2);
