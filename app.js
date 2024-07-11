5c6cc2672c7cec683e24af138c14a3d703b1c5f9
xenongee <rublgleb@gmail.com>
2024-07-11 15:40:43 +0300

    Example of scope chain

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* 
     Strict mode
-*/
 
 'use strict';
 
@@ -17,3 +16,40 @@ function a(b, b) { // error with strict mode, because: Duplicate parameter name 
 }
 
 a(1,1);
+*/
+
+/*
+    Пример scope chain
+    Example of scope chain
+*/
+
+'use strict'
+
+let successMsg = 'Success!';
+const user = {
+    name: 'John',
+    roles: []
+}
+
+function addRole(user, role) {
+    if (role === 'admin') {
+        console.warn('Cannot add admin role');
+        // console.log(a); // a is not defined (a in another scope)
+        return user;
+    }
+    user.roles.push(role);
+    // console.log(successMsg1); // successMsg1 is not defined
+    let successMsg = 'Greatings!';
+    console.log(successMsg);
+    
+    function logRoles() {
+        const a = 3;
+        console.log(user.roles);
+    }
+    
+    logRoles()
+    return user;
+}
+
+console.log(addRole(user, 'admin'));
+console.log(addRole(user, 'redactor'));
