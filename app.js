91365106a60303b9c297556457d756ad7939a301
xenongee <rublgleb@gmail.com>
2024-07-22 15:24:05 +0300

    Bind

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ console.log(b.getAnew());
 
 /*
     Call, apply
-*/
 
 'use strict';
 
@@ -62,3 +61,32 @@ addDamageFunc.call(ford, 'steering', 10);
 addDamageFunc.call(toyota, 'chassis', 8);
 addDamageFunc.apply(ford, ['lights', 7]);
 addDamageFunc.apply(toyota, ['suspension', 2]);
+*/
+
+/*
+    Bind
+*/
+
+const automobile = {
+    brand: 'Ford',
+    model: 'Mustang',
+    damages: [],
+}
+
+const carManipulation = {
+    addDamage(part, rate) {
+        console.log(`on auto ${this.brand} ${this.model} ${this.year} added damage '${part}' with ${rate} damage rate`);
+        this.damages.push({
+            part,
+            rate
+        });
+    }
+}
+
+const addDamageFord = carManipulation.addDamage.bind(automobile);
+addDamageFord('wheels', 2);
+addDamageFord('brakes', 3);
+addDamageFord('bumper', 4);
+const addDamageFord_Roof = carManipulation.addDamage.bind(automobile, 'roof');
+addDamageFord_Roof(9);
+console.log(automobile);
