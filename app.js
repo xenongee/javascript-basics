c6825985062a1110a204b109d63208e8980f161c
xenongee <rublgleb@gmail.com>
2024-07-23 16:15:33 +0300

    Working with styles and classes

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ panel.value = 'Hello everyone!';
 
 /*
     Keyboard and Click handling
+    
 */
-
 function changePanel() {
     const input = document.querySelector('.input');
     const panel = document.querySelector('.panel');
@@ -23,6 +23,7 @@ function changePanel() {
     // console.log(input.value);
     panel.innerHTML = input.value;
     input.value = '';
+    notify(true);
 }
 
 document.querySelector('.button').addEventListener('click', function () {
@@ -35,3 +36,20 @@ document.querySelector('.input').addEventListener('keydown', function (event) {
         changePanel();
     }
 });
+
+
+/*
+    Working with styles and classes
+*/
+
+function notify(status) {
+    const notify = document.querySelector('.notify');
+    // notify.style.display = 'block';
+    if (status) {
+        notify.classList.add('notify-active');
+    } else {
+        notify.classList.remove('notify-active');
+    }
+}
+
+notify(false);
