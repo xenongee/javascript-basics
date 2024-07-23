185cd55e0927e4390e4c8ef2a94098638501fb9e
xenongee <rublgleb@gmail.com>
2024-07-23 15:56:02 +0300

    Click handling

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /*
     DOM API
     Selecting and manipulating elements
-*/
 
 const panel = document.querySelector('.panel')
 
@@ -9,3 +8,27 @@ console.log(panel.innerHTML);
 
 panel.innerText = 'Hello World!';
 panel.value = 'Hello everyone!';
+*/
+
+/*
+    Click handling
+*/
+
+`use strict`;
+
+document.querySelector('.button').addEventListener('click', function () {
+    changePanel();
+})
+
+function changePanel() {
+    const input = document.querySelector('.input');
+    const panel = document.querySelector('.panel');
+
+    if (!input.value) return;
+
+    // console.log(input.value);
+    panel.innerHTML = input.value;
+    input.value = '';
+}
+
+
