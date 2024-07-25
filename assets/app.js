dc8019f6b23e20c1426a7589c7e12e572d878c84
xenongee <rublgleb@gmail.com>
2024-07-25 18:09:58 +0300

    habbit app - navmenu

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,102 @@
 `use strict`;
 
-
 const habbitKey = 'HABBIT_DATA';
-let habbits = [];
-const modal = document.querySelector('.modal');
+let habbitsData = [];
+const page = {
+    sidePanel: {
+        menu: document.querySelector('.habbitsMenu'),
+    },
+    mainPanel: {
+        header: document.querySelector('.habbitName'),
+        progress: document.querySelector('.habbitProgress'),
+        taskList: document.querySelector('.commentList'),
+        currentDay: document.querySelector('.commentCurrentDay'),
+    },
+    modal: document.querySelector('.modal'),
+};
 
+const templates = {
+    menuElement: function(id, icon, name) {
+        const elem = document.createElement('span');
+        elem.setAttribute('habbit-menu-id', id);
+        elem.innerHTML = `${icon} <a>${name}</a>`;
+        return elem;
+    }
+}
+
+const habbitIcons = {
+    sport: '🏆',
+    food: '🥗',
+    edu: '📚'
+};
+
+
+/*
+    Data
+*/
 
 function saveData() {
-    localStorage.setItem(habbitKey, JSON.stringify(habbits));
+    localStorage.setItem(habbitKey, JSON.stringify(habbitsData));
 }
 
 function loadData() {
-    const habbitData = JSON.parse(localStorage.getItem(habbitKey));
-    if (Array.isArray(habbitData)) {
-        habbits = habbitData;
+    const data = JSON.parse(localStorage.getItem(habbitKey));
+    if (Array.isArray(data)) {
+        habbitsData = data;
     }
 }
 
 
+/* 
+    Render 
+*/
+
 function toggleModal() {
-    modal.classList.toggle('hidden');
+    page.modal.classList.toggle('hidden');
+}
+
+function renderSidePanel(activeId) {
+    const activeElem = habbitsData.find(habbitElem => habbitElem.id === activeId) ?? 0;
+
+    for(const habbit of habbitsData) {
+        // console.log('elem:', index, habbit);
+        // exists elem
+        const existsElem = page.sidePanel.menu.querySelector(`[habbit-menu-id="${habbit.id}"]`);
+        // create elem
+        if (!existsElem)  {
+            const newElement = templates.menuElement(habbit.id, habbitIcons[habbit.icon], habbit.name);
+            newElement.addEventListener('click', () => renderPage(habbit.id));
+            if (habbit.id === activeElem.id) {
+                newElement.classList.add('active');
+            }
+            page.sidePanel.menu.appendChild(newElement);
+            continue;
+        }
+        // active-class
+        if (habbit.id === activeElem.id) {
+            existsElem.classList.add('active');
+        } else {
+            existsElem.classList.remove('active');
+        }
+    }
+}
+
+function renderMainPanel(activeId) {
+
+}
+
+function renderPage(activeId) {
+    console.log('activeId:', activeId);
+    renderSidePanel(activeId);
+    renderMainPanel(activeId)
 }
 
 
-(()=>{
-    loadData();
+/*
+    Init
+*/
 
-    console.log(habbits);
+(() => {
+    loadData();
+    renderPage(0);
 })()
