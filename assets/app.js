a80e414702a92e56d3873f31c9443eddbac60006
xenongee <rublgleb@gmail.com>
2024-07-26 18:16:38 +0300

    Habit App - add day with comment

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,16 @@
 `use strict`;
 
-const habbitKey = 'HABBIT_DATA';
-let habbitsData = [];
+const habitKey = 'HABIT_DATA';
+let habitsData = [];
+let globalActiveHabit = 0;
 const page = {
     sidePanel: {
-        menu: document.querySelector('.habbitsMenu'),
+        menu: document.querySelector('.habitsMenu'),
     },
     mainPanel: {
-        header: document.querySelector('.habbitName'),
-        progress: document.querySelector('.habbitProgress'),
-        progressValue: document.querySelector('.habbitProgress progress'),
+        header: document.querySelector('.habitName'),
+        progress: document.querySelector('.habitProgress'),
+        progressValue: document.querySelector('.habitProgress progress'),
         main: document.querySelector('.mainPanel main'),
         daysList: document.querySelector('.daysList'),
         currentDay: document.querySelector('.currentDay'),
@@ -20,16 +21,16 @@ const page = {
 const templates = {
     menuElement: function (id, icon, name) {
         const elem = document.createElement('span');
-        elem.setAttribute('habbit-menu-id', id);
+        elem.setAttribute('habit-menu-id', id);
         elem.innerHTML = `${icon} <a>${name}</a>`;
         return elem;
     },
-    habbitHeader: function (name) {
+    habitHeader: function (name) {
         const elem = document.createElement('strong');
         elem.innerHTML = name;
         return elem;
     },
-    habbitProgress: function (value, max) {
+    habitProgress: function (value, max) {
         const parent = document.createElement('div');
         const spanElem = document.createElement('span')
         const progressElem = document.createElement('progress')
@@ -39,7 +40,7 @@ const templates = {
         parent.append(spanElem, progressElem);
         return parent.children;
     },
-    habbitDay: function (dayCount, comment) {
+    habitDay: function (dayCount, comment) {
         const elem = document.createElement('li');
         elem.innerHTML = `<strong>Day ${dayCount}</strong><div class="comment"><i>${comment}</i><button onclick="removeComment()">Remove</button></div>`
         return elem;
@@ -47,7 +48,7 @@ const templates = {
 
 }
 
-const habbitIcons = {
+const habitIcons = {
     sport: '🏆',
     food: '🥗',
     edu: '📚'
@@ -59,16 +60,48 @@ const habbitIcons = {
 */
 
 function saveData() {
-    localStorage.setItem(habbitKey, JSON.stringify(habbitsData));
+    localStorage.setItem(habitKey, JSON.stringify(habitsData));
 }
 
 function loadData() {
-    const data = JSON.parse(localStorage.getItem(habbitKey));
+    const data = JSON.parse(localStorage.getItem(habitKey));
     if (Array.isArray(data)) {
-        habbitsData = data;
+        habitsData = data;
     }
 }
 
+function addDay(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(event.target);
+    const comment = data.get('comment');
+    if (!comment) {
+        form['comment'].classList.add('error');
+        return;
+    }
+    if (form['comment'].classList.contains('error')) {
+        form['comment'].classList.remove('error');
+    }
+    console.log(habitsData);
+    habitsData = habitsData.map(habbit => {
+        if (habbit.id === globalActiveHabit) {
+            return {
+                ...habbit,
+                days: habbit.days.concat([{comment}])
+            }
+        }
+        return habbit;
+    })
+    console.log(habitsData);
+    form['comment'].value = '';
+    renderPage(globalActiveHabit);
+    saveData();
+}
+
+function addhabit(event) {
+    console.log(event);
+    event.preventDefault();
+}
 
 /* 
     Render 
@@ -78,17 +111,17 @@ function toggleModal() {
     page.modal.classList.toggle('hidden');
 }
 
-function renderSidePanel(currentHabbit) {
-    for (const habbit of habbitsData) {
+function renderSidePanel(currenthabit) {
+    for (const habit of habitsData) {
         // exists elem
-        const existsElem = page.sidePanel.menu.querySelector(`[habbit-menu-id="${habbit.id}"]`);
+        const existsElem = page.sidePanel.menu.querySelector(`[habit-menu-id="${habit.id}"]`);
 
         // if not exist - create elem
         if (!existsElem) {
-            const newElement = templates.menuElement(habbit.id, habbitIcons[habbit.icon], habbit.name);
-            newElement.addEventListener('click', () => renderPage(habbit.id));
+            const newElement = templates.menuElement(habit.id, habitIcons[habit.icon], habit.name);
+            newElement.addEventListener('click', () => renderPage(habit.id));
 
-            if (habbit.id === currentHabbit.id) {
+            if (habit.id === currenthabit.id) {
                 newElement.classList.add('active');
             }
 
@@ -99,7 +132,7 @@ function renderSidePanel(currentHabbit) {
         }
 
         // if exist - toggle 'active' class
-        if (habbit.id === currentHabbit.id) {
+        if (habit.id === currenthabit.id) {
             existsElem.classList.add('active');
         } else {
             existsElem.classList.remove('active');
@@ -107,37 +140,40 @@ function renderSidePanel(currentHabbit) {
     }
 }
 
-function renderMainPanel(currentHabbit) {
-    if (!currentHabbit) {
+function renderMainPanel(currenthabit) {
+    if (!currenthabit) {
         return;
     }
 
-    const progressMax = currentHabbit.target;
-    const progressValue = currentHabbit.days.length > progressMax ? progressMax : currentHabbit.days.length;
+    const progressMax = currenthabit.target;
+    const progressValue = currenthabit.days.length > progressMax ? progressMax : currenthabit.days.length;
 
-    // header of habbit and its progress
-    page.mainPanel.header.replaceChildren(templates.habbitHeader(currentHabbit.name));
-    page.mainPanel.progress.replaceChildren(...templates.habbitProgress(progressValue, progressMax));
+    // header of habit and its progress
+    page.mainPanel.header.replaceChildren(templates.habitHeader(currenthabit.name));
+    page.mainPanel.progress.replaceChildren(...templates.habitProgress(progressValue, progressMax));
 
-    // main of habbit with days of tasks completed
+    // main of habit with days of tasks completed
     page.mainPanel.daysList.innerHTML = '';
-    for (const index in currentHabbit.days) {
-        page.mainPanel.daysList.appendChild(templates.habbitDay(Number(index) + 1, currentHabbit.days[index].comment));
+    for (const index in currenthabit.days) {
+        page.mainPanel.daysList.appendChild(templates.habitDay(Number(index) + 1, currenthabit.days[index].comment));
     }
 
-    page.mainPanel.currentDay.innerHTML = `Day ${currentHabbit.days.length + 1}`;
+    page.mainPanel.currentDay.innerHTML = `Day ${currenthabit.days.length + 1}`;
 
-    // unhide main panel of habbit
+    // unhide main panel of habit
     if (page.mainPanel.main.classList.contains('hidden')) {
         page.mainPanel.main.classList.remove('hidden');
     }
 }
 
-function renderPage(activeHabbitId) {
-    const activeHabbit = habbitsData.find(habbitElem => habbitElem.id === activeHabbitId) ?? 0;
+function renderPage(activeHabitId) {
+    const activeHabit = habitsData.find(habitElem => habitElem.id === activeHabitId) ?? 0;
+    globalActiveHabit = activeHabit.id;
 
-    renderSidePanel(activeHabbit);
-    renderMainPanel(activeHabbit)
+    // console.log(habitsData);
+
+    renderSidePanel(activeHabit);
+    renderMainPanel(activeHabit);
 }
 
 
@@ -147,5 +183,5 @@ function renderPage(activeHabbitId) {
 
 (() => {
     loadData();
-    renderPage();
+    renderPage(1);
 })()
